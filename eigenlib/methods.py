6778851e05f7bf98c3f325fce4e1cb864272e5ab
dtredu <105952812+dtredu@users.noreply.github.com>
2023-12-18 13:23:28 +0300

    Add files via upload

--- eigenlib/methods.py
+++ eigenlib/methods.py
@@ -1,22 +1,22 @@
 import numpy as np
 
-def power_iteration(A,epsilon = 1e-6, iter_limit = 1e3):
+def power_iteration(A,epsilon = 1e-16, iter_limit = 1e3):
   assert A.shape[0] == A.shape[1]
   n = A.shape[0]
-  last_x = np.random.rand(n)
-  x = A @ last_x
-  c = x[0] / last_x[0]
-  last_c = c + epsilon * 2
+  x = np.random.rand(n)
+  c = 1e9
+  last_c = -1e9
   i = 1
-  while np.abs(c - last_c) > epsilon:
+  while True:
     i+=1
-    if i >= iter_limit:
+    if np.abs(c - last_c) <= epsilon:
+      return c,x
+    if i > iter_limit:
       return None
     last_x = x
     last_c = c
     x = A @ last_x
-    c = x[0] / last_x[0]
+    #c = x[0] / last_x[0]
+    #c = x.T @ A @ x / (x.T @ x)
+    c = np.dot(x,last_x) / np.dot(last_x,last_x)
     x /= np.linalg.norm(x)
-    print(c,x)
-  return c,x
-
