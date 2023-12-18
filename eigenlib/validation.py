de243dcfc73453a343e05d8c907ac383a02beb0b
dtredu <105952812+dtredu@users.noreply.github.com>
2023-12-18 12:04:13 +0300

    upload 1

---
+++ eigenlib/validation.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def validate(A,eigenvalues, eigenvectors, epsilon=1e-6):
+  n = len(eigenvalues)
+  correct = [True] * n
+  allcorrect = True
+  for i in range(n):
+    vec = (A @ eigenvectors[i]) / eigenvalues[i]
+    diff = vec - eigenvectors[i]
+    mx = np.max(np.abs(diff))
+    if mx > epsilon:
+      correct[i] = False
+      allcorrect = False
+  return allcorrect,correct
